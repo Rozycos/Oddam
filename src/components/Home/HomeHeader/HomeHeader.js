e65970be908a01c54255970cf49b3dc7d54a87d8
rozycos <rozycos@gmail.com>
2023-04-24 21:39:14 +0200

    fixed resolution bug

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -9,12 +9,14 @@ const HomeHeader = () => {
             <div>
                 <Navigation />
             </div>
-            <div className="hero__image"></div>
-            <div className="hero__text">
-                <h1><p>Zacznij pomagać!</p> Oddaj niechciane rzeczy w zaufane ręce</h1>
-                <button className="btn"><Link className="btn__primary" to="/logowanie">oddaj <br/>rzeczy</Link></button>
-                <button className="btn"><Link className="btn__primary" to="/logowanie">zorganizuj zbiórkę</Link></button>
-            </div>
+            <section className="hero__section">
+                <div className="hero__image"></div>
+                <div className="hero__text">
+                    <h1><p>Zacznij pomagać!</p> Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <button className="btn"><Link className="btn__primary" to="/logowanie">oddaj <br/>rzeczy</Link></button>
+                    <button className="btn"><Link className="btn__primary" to="/logowanie">zorganizuj zbiórkę</Link></button>
+                </div>
+            </section>
         </>
     )
 }
